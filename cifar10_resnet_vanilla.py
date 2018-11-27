f4f4be878eeb9e3b1c38c4f7bf994dadce3e7383
Naiqi Li <lnq18@mails.tsinghua.edu.cn>
2018-11-27 13:30:36 +0800

    Create cifar10_resnet_vanilla.py

---
+++ cifar10_resnet_vanilla.py
@@ -0,0 +1,230 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.dataset import random_split
+from tensorboardX import SummaryWriter
+
+import os
+
+'''
+https://github.com/KellerJordan/ResNet-PyTorch-CIFAR10
+'''
+
+class ResNet(nn.Module):
+    
+    def __init__(self, n=7, res_option='A', use_dropout=False):
+        super(ResNet, self).__init__()
+        self.res_option = res_option
+        self.use_dropout = use_dropout
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.layers1 = self._make_layer(n, 16, 16, 1)
+        self.layers2 = self._make_layer(n, 32, 16, 2)
+        self.layers3 = self._make_layer(n, 64, 32, 2)
+        self.avgpool = nn.AvgPool2d(8)
+        self.linear = nn.Linear(64, 10)
+    
+    def _make_layer(self, layer_count, channels, channels_in, stride):
+        return nn.Sequential(
+            ResBlock(channels, channels_in, stride, res_option=self.res_option, use_dropout=self.use_dropout),
+            *[ResBlock(channels) for _ in range(layer_count-1)])
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.norm1(out)
+        out = self.relu1(out)
+        out = self.layers1(out)
+        out = self.layers2(out)
+        out = self.layers3(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+class ResBlock(nn.Module):
+    
+    def __init__(self, num_filters, channels_in=None, stride=1, res_option='A', use_dropout=False):
+        super(ResBlock, self).__init__()
+        
+        # uses 1x1 convolutions for downsampling
+        if not channels_in or channels_in == num_filters:
+            channels_in = num_filters
+            self.projection = None
+        else:
+            if res_option == 'A':
+                self.projection = IdentityPadding(num_filters, channels_in, stride)
+            elif res_option == 'B':
+                self.projection = ConvProjection(num_filters, channels_in, stride)
+            elif res_option == 'C':
+                self.projection = AvgPoolPadding(num_filters, channels_in, stride)
+        self.use_dropout = use_dropout
+
+        self.conv1 = nn.Conv2d(channels_in, num_filters, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_filters)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_filters)
+        if self.use_dropout:
+            self.dropout = nn.Dropout(inplace=True)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.use_dropout:
+            out = self.dropout(out)
+        if self.projection:
+            residual = self.projection(x)
+        out += residual
+        out = self.relu2(out)
+        return out
+
+
+# various projection options to change number of filters in residual connection
+# option A from paper
+class IdentityPadding(nn.Module):
+    def __init__(self, num_filters, channels_in, stride):
+        super(IdentityPadding, self).__init__()
+        # with kernel_size=1, max pooling is equivalent to identity mapping with stride
+        self.identity = nn.MaxPool2d(1, stride=stride)
+        self.num_zeros = num_filters - channels_in
+    
+    def forward(self, x):
+        out = F.pad(x, (0, 0, 0, 0, 0, self.num_zeros))
+        out = self.identity(out)
+        return out
+
+# option B from paper
+class ConvProjection(nn.Module):
+
+    def __init__(self, num_filters, channels_in, stride):
+        super(ResA, self).__init__()
+        self.conv = nn.Conv2d(channels_in, num_filters, kernel_size=1, stride=stride)
+    
+    def forward(self, x):
+        out = self.conv(x)
+        return out
+
+# experimental option C
+class AvgPoolPadding(nn.Module):
+
+    def __init__(self, num_filters, channels_in, stride):
+        super(AvgPoolPadding, self).__init__()
+        self.identity = nn.AvgPool2d(stride, stride=stride)
+        self.num_zeros = num_filters - channels_in
+    
+    def forward(self, x):
+        out = F.pad(x, (0, 0, 0, 0, 0, self.num_zeros))
+        out = self.identity(out)
+        return out
+		
+from datetime import datetime
+now = datetime.now()
+time_suffix = 'Vanilla_epoch100_' + now.strftime("%Y%m%d-%H%M%S")
+
+dataroot = 'd:/lab/dataset/'
+b_size = 200
+n_epochs = 100
+lr = 0.01
+best_loss = 100000
+n_iter = 0
+test_step = 100
+model_file = './tmp/model1_%s.md' % time_suffix
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+writer_train = SummaryWriter('./tmp/log_train_%s' % time_suffix)
+writer_val = SummaryWriter('./tmp/log_test_%s' % time_suffix)
+
+def test_validate(model, device, test_loader, test_valid='Test'):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += nn.CrossEntropyLoss(reduction='sum')(output, target).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    acc = 100. * correct / len(test_loader.dataset)
+    writer_val.add_scalar('Loss', test_loss, n_iter)
+    writer_val.add_scalar('Acc', acc, n_iter)
+    print('\n{} set: Average loss: {:.6f}, Accuracy: {}/{} ({:.6f}%)\n'.format(
+        test_valid, test_loss, correct, len(test_loader.dataset), acc))
+    return test_loss
+    
+def train(model, device, train_loader, test_loader, optimizer, epoch):
+    global best_loss, patience, n_iter
+    batch_idx = 0
+    for data, target in train_loader:
+        model.train()
+        batch_idx += 1
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.CrossEntropyLoss()(output, target)
+        loss.backward()
+        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct = pred.eq(target.view_as(pred)).sum().item()
+        acc = 100. * correct / b_size
+        writer_train.add_scalar('Loss', loss.item(), n_iter)
+        writer_train.add_scalar('Acc', acc, n_iter)
+        n_iter += 1
+        optimizer.step()
+        if batch_idx % 1 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+        if n_iter % test_step == 0:
+            print('Start testing...')
+            test_loss = test_validate(model, device, test_loader)
+
+def main():        
+    dataroot = 'D:\Lab\dataset'
+    
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    train_dataset = torchvision.datasets.CIFAR10(root=dataroot, train=True, download=False, transform=transform_train)
+    test_dataset = torchvision.datasets.CIFAR10(root=dataroot, train=False, download=False, transform=transform_test)
+    
+    train_size = len(train_dataset)
+    
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=b_size, shuffle=True, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=b_size, shuffle=False, num_workers=2)
+
+    model = ResNet(9, res_option='A', use_dropout=False).to(device)
+    print(model)
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
+
+    for epoch in range(1, n_epochs + 1):
+        train(model, device, train_loader, test_loader, optimizer, epoch)          
+        
+main()
+writer_train.close()
+writer_val.close()
