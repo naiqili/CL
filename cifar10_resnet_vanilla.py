ad424df0b5c3314c71caa15703aef0a5a5306935
Naiqi Li <lnq18@mails.tsinghua.edu.cn>
2018-11-28 11:17:58 +0800

    change model src

--- cifar10_resnet_vanilla.py
+++ cifar10_resnet_vanilla.py
@@ -8,133 +8,17 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import random_split
 from tensorboardX import SummaryWriter
+from cifar10_model import *
 
 import os
 
-'''
-https://github.com/KellerJordan/ResNet-PyTorch-CIFAR10
-'''
-
-class ResNet(nn.Module):
-    
-    def __init__(self, n=7, res_option='A', use_dropout=False):
-        super(ResNet, self).__init__()
-        self.res_option = res_option
-        self.use_dropout = use_dropout
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.norm1 = nn.BatchNorm2d(16)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.layers1 = self._make_layer(n, 16, 16, 1)
-        self.layers2 = self._make_layer(n, 32, 16, 2)
-        self.layers3 = self._make_layer(n, 64, 32, 2)
-        self.avgpool = nn.AvgPool2d(8)
-        self.linear = nn.Linear(64, 10)
-    
-    def _make_layer(self, layer_count, channels, channels_in, stride):
-        return nn.Sequential(
-            ResBlock(channels, channels_in, stride, res_option=self.res_option, use_dropout=self.use_dropout),
-            *[ResBlock(channels) for _ in range(layer_count-1)])
-    
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.norm1(out)
-        out = self.relu1(out)
-        out = self.layers1(out)
-        out = self.layers2(out)
-        out = self.layers3(out)
-        out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-class ResBlock(nn.Module):
-    
-    def __init__(self, num_filters, channels_in=None, stride=1, res_option='A', use_dropout=False):
-        super(ResBlock, self).__init__()
-        
-        # uses 1x1 convolutions for downsampling
-        if not channels_in or channels_in == num_filters:
-            channels_in = num_filters
-            self.projection = None
-        else:
-            if res_option == 'A':
-                self.projection = IdentityPadding(num_filters, channels_in, stride)
-            elif res_option == 'B':
-                self.projection = ConvProjection(num_filters, channels_in, stride)
-            elif res_option == 'C':
-                self.projection = AvgPoolPadding(num_filters, channels_in, stride)
-        self.use_dropout = use_dropout
-
-        self.conv1 = nn.Conv2d(channels_in, num_filters, kernel_size=3, stride=stride, padding=1)
-        self.bn1 = nn.BatchNorm2d(num_filters)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(num_filters)
-        if self.use_dropout:
-            self.dropout = nn.Dropout(inplace=True)
-        self.relu2 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu1(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        if self.use_dropout:
-            out = self.dropout(out)
-        if self.projection:
-            residual = self.projection(x)
-        out += residual
-        out = self.relu2(out)
-        return out
-
-
-# various projection options to change number of filters in residual connection
-# option A from paper
-class IdentityPadding(nn.Module):
-    def __init__(self, num_filters, channels_in, stride):
-        super(IdentityPadding, self).__init__()
-        # with kernel_size=1, max pooling is equivalent to identity mapping with stride
-        self.identity = nn.MaxPool2d(1, stride=stride)
-        self.num_zeros = num_filters - channels_in
-    
-    def forward(self, x):
-        out = F.pad(x, (0, 0, 0, 0, 0, self.num_zeros))
-        out = self.identity(out)
-        return out
-
-# option B from paper
-class ConvProjection(nn.Module):
-
-    def __init__(self, num_filters, channels_in, stride):
-        super(ResA, self).__init__()
-        self.conv = nn.Conv2d(channels_in, num_filters, kernel_size=1, stride=stride)
-    
-    def forward(self, x):
-        out = self.conv(x)
-        return out
-
-# experimental option C
-class AvgPoolPadding(nn.Module):
-
-    def __init__(self, num_filters, channels_in, stride):
-        super(AvgPoolPadding, self).__init__()
-        self.identity = nn.AvgPool2d(stride, stride=stride)
-        self.num_zeros = num_filters - channels_in
-    
-    def forward(self, x):
-        out = F.pad(x, (0, 0, 0, 0, 0, self.num_zeros))
-        out = self.identity(out)
-        return out
-		
 from datetime import datetime
 now = datetime.now()
-time_suffix = 'Vanilla_epoch200_' + now.strftime("%Y%m%d-%H%M%S")
+time_suffix = 'Vanilla_epoch150_' + now.strftime("%Y%m%d-%H%M%S")
 
 dataroot = '~/dataset/'
 b_size = 200
-n_epochs = 200
+n_epochs = 150
 lr = 0.01
 best_loss = 100000
 n_iter = 0
@@ -218,9 +102,9 @@ def main():
         test_dataset,
         batch_size=b_size, shuffle=False, num_workers=2)
 
-    model = ResNet(9, res_option='A', use_dropout=True).to(device)
+    model = ResNet18().to(device)
     print(model)
-    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.0001)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
     for epoch in range(1, n_epochs + 1):
         train(model, device, train_loader, test_loader, optimizer, epoch)          
